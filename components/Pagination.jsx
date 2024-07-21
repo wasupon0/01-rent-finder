d9faf2efd44a6d257808a3775878d63c71702090
wasupon <wasupon.mail@gmail.com>
2024-07-21 12:06:05 +0900

    feat: prev next hover bg

--- components/Pagination.jsx
+++ components/Pagination.jsx
@@ -11,7 +11,7 @@ const Pagination = ({ page, pageSize, totalItems, onPageChange }) => {
   return (
     <section className="container flex items-center justify-center mx-auto my-8">
       <button
-        className="px-2 py-1 mr-2 border border-gray-300 rounded"
+        className="px-2 py-1 mr-2 border border-gray-300 rounded hover:bg-orange-200"
         disabled={page === 1}
         onClick={() => handlePageChange(page - 1)}
       >
@@ -21,7 +21,7 @@ const Pagination = ({ page, pageSize, totalItems, onPageChange }) => {
         Page {page} of {totalPages}
       </span>
       <button
-        className="px-2 py-1 ml-2 border border-gray-300 rounded"
+        className="px-2 py-1 ml-2 border border-gray-300 rounded hover:bg-orange-200"
         disabled={page === totalPages}
         onClick={() => handlePageChange(page + 1)}
       >
