591149f9c5363ec074f379a3c315df80bbcd463a
wasupon <wasupon.mail@gmail.com>
2024-07-21 12:18:35 +0900

    fix: prev next show/hide logic

--- components/Pagination.jsx
+++ components/Pagination.jsx
@@ -10,23 +10,27 @@ const Pagination = ({ page, pageSize, totalItems, onPageChange }) => {
 
   return (
     <section className="container flex items-center justify-center mx-auto my-8">
-      <button
-        className="px-2 py-1 mr-2 border border-gray-300 rounded hover:bg-orange-200"
-        disabled={page === 1}
-        onClick={() => handlePageChange(page - 1)}
-      >
-        Previous
-      </button>
+      {page !== 1 && (
+        <button
+          className="px-2 py-1 mr-2 border border-gray-300 rounded hover:bg-orange-200"
+          disabled={page === 1}
+          onClick={() => handlePageChange(page - 1)}
+        >
+          Previous
+        </button>
+      )}
       <span className="mx-2">
         Page {page} of {totalPages}
       </span>
-      <button
-        className="px-2 py-1 ml-2 border border-gray-300 rounded hover:bg-orange-200"
-        disabled={page === totalPages}
-        onClick={() => handlePageChange(page + 1)}
-      >
-        Next
-      </button>
+      {page !== totalPages && (
+        <button
+          className="px-2 py-1 ml-2 border border-gray-300 rounded hover:bg-orange-200"
+          disabled={page === totalPages}
+          onClick={() => handlePageChange(page + 1)}
+        >
+          Next
+        </button>
+      )}
     </section>
   );
 };
